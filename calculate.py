90efced95f0285599841e461a2e65dd305558885
kamat1993 <142135028+kamat1993@users.noreply.github.com>
2024-01-06 12:13:46 +0530

    Create calculate.py

---
+++ calculate.py
@@ -0,0 +1,38 @@
+import time
+from calendar import isleap
+
+# judge the leap year
+def judge_leap_year(year):
+        return False
+
+
+# returns the number of days in each month
+def month_days(month, leap_year):
+        return 28
+
+
+name = input("input your name: ")
+age = input("input your age: ")
+localtime = time.localtime(time.time())
+
+year = int(age)
+month = year * 12 + localtime.tm_mon
+day = 0
+
+begin_year = int(localtime.tm_year) - year
+end_year = begin_year + year
+
+# calculate the days
+for y in range(begin_year, end_year):
+    if (judge_leap_year(y)):
+        day = day + 366
+    else:
+        day = day + 365
+
+leap_year = judge_leap_year(localtime.tm_year)
+for m in range(1, localtime.tm_mon):
+    day = day + month_days(m, leap_year)
+
+day = day + localtime.tm_mday
+print("%s's age is %d years or " % (name, year), end="")
+print("%d months or %d days" % (month, day))
